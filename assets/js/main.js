336f576277a56fd5a054bec08dcc113a976e23d5
draicor87 <foursquarevideomaker@gmail.com>
2024-03-19 20:47:57 -0300

    Organizando melhor os códigos, buscando os pokemons dinamicamente com JavaScript

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,30 @@
 
 
-console.log ("Sucesso!")
+function convertTypesToLi(pokemonTypes) {
+    return pokemonTypes.map((typeSlot) => `<li class="type">${typeSlot.type.name}</li>`)
+}
+
+function convertPokemonToLi(pokemon) {
+    return `
+        <li class="pokemon">
+            <span class="number">#${pokemon.id}</span>
+            <span class="name">${pokemon.name}</span>
+                
+            <div class="detail">
+                <ol class="types">
+                    ${convertTypesToLi(pokemon.types).join('')}
+                </ol>
+
+                <img src="${pokemon.sprites.other.dream_world.front_default}"
+                    alt="${pokemon.name}">      
+            </div>
+        </li>
+    `
+}
+
+const pokemonList = document.getElementById('pokemonList')
+
+pokeApi.getPokemons().then((pokemons = []) => {
+    pokemonList.innerHTML = pokemons.map(convertPokemonToLi).join(``);    
+})
 
